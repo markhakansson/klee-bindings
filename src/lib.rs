496d4ade0d0113ffdc34f35cefb12e0a1fa26134
Mark Hakansson <mark.hakansson@live.se>
2021-04-13 09:23:23 +0200

    Add github workflows and make crate ready to publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! Raw Rust bindings to KLEE.
+//!
+//! This crate can be used to create Rust bindings to KLEE using bindgen.
+//! It provides unsafe FFI to the local KLEE library. Works without the
+//! standard library, but you will need the `cty` crate to use it.
+//!
+//! Please refer to the official KLEE documentation on how to use it.
 #![no_std]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
